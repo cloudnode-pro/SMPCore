e0d7a71005bd94389d4a9909b39b71258ebb12cd
Zefir Kirilov <contact@zefir.pro>
2024-05-13 11:42:32 +0300

    plugin permissions

---
+++ src/main/java/pro/cloudnode/smp/smpcore/Permission.java
@@ -0,0 +1,8 @@
+package pro.cloudnode.smp.smpcore;
+
+import org.jetbrains.annotations.NotNull;
+
+public final class Permission {
+    public static @NotNull String RELOAD = "smpcore.reload";
+    public static @NotNull String BAN = "smpcore.ban";
+}
