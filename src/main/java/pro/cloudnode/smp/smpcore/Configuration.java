2cc5d1a8a08ae431484527d43b61a44aaec85183
Zefir Kirilov <contact@zefir.pro>
2025-12-26 23:25:15 +0200

    Added optional duration for bans to enable temporary bans

--- src/main/java/pro/cloudnode/smp/smpcore/Configuration.java
+++ src/main/java/pro/cloudnode/smp/smpcore/Configuration.java
@@ -42,7 +42,7 @@ public final class Configuration extends BaseConfig {
         return config.getInt("join.request-expire-minutes");
     }
 
-    public @NotNull Component relativeTime(final int t, final @NotNull ChronoUnit unit) {
+    public @NotNull Component relativeTime(final Number t, final @NotNull ChronoUnit unit) {
         final @NotNull String formatString = Objects.requireNonNull(config.getString("relative-time." + switch (unit) {
             case SECONDS -> "seconds";
             case MINUTES -> "minutes";
@@ -55,7 +55,7 @@ public final class Configuration extends BaseConfig {
             }
         }));
         return MiniMessage.miniMessage()
-                .deserialize(formatString, Formatter.number("t", t), Formatter.choice("format", Math.abs(t)));
+                .deserialize(formatString, Formatter.number("t", t), Formatter.choice("format", Math.abs(t.doubleValue())));
     }
 
     public @NotNull Component relativeTimeFuture(final @NotNull Component relativeTime) {
@@ -66,5 +66,14 @@ public final class Configuration extends BaseConfig {
     public @NotNull Component relativeTimePast(final @NotNull Component relativeTime) {
         return MiniMessage.miniMessage()
                 .deserialize(Objects.requireNonNull(config.getString("relative-time.past")), Placeholder.component("t", relativeTime));
+    }
+
+    public @NotNull Component relativeTimeDuration(final @NotNull Component duration) {
+        return MiniMessage.miniMessage()
+                .deserialize(Objects.requireNonNull(config.getString("relative-time.duration")), Placeholder.component("t", duration));
+    }
+
+    public @NotNull Component relativeTimeDurationIndefinite() {
+        return MiniMessage.miniMessage().deserialize(Objects.requireNonNull(config.getString("relative-time.duration-indefinite")));
     }
 }
--- src/main/java/pro/cloudnode/smp/smpcore/command/BanCommand.java
+++ src/main/java/pro/cloudnode/smp/smpcore/command/BanCommand.java
@@ -10,6 +10,9 @@ import pro.cloudnode.smp.smpcore.Member;
 import pro.cloudnode.smp.smpcore.Permission;
 import pro.cloudnode.smp.smpcore.SMPCore;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
@@ -19,38 +22,49 @@ import java.util.Optional;
 
 public final class BanCommand extends Command {
     /**
-     * Usage: {@code /<command> <username> [reason]}
+     * Usage: {@code /<command> <username> [duration] [reason]}
      */
     @Override
     public boolean run(@NotNull CommandSender sender, @NotNull String label, @NotNull String @NotNull [] args) {
         if (!sender.hasPermission(Permission.BAN))
             return sendMessage(sender, SMPCore.messages().errorNoPermission());
-        if (args.length < 1) return sendMessage(sender, SMPCore.messages().usage(label, "<username> [reason]"));
-        final @NotNull OfflinePlayer target = SMPCore.getInstance().getServer().getOfflinePlayer(args[0]);
+        if (args.length < 1) return sendMessage(sender, SMPCore.messages().usage(label, "<username> [duration] [reason]"));
 
-        final @Nullable String reason = args.length > 1 ? String.join(" ", Arrays.copyOfRange(args, 1, args.length)) : null;
-        final @Nullable Date banExpiry = null;
+        final @Nullable String durationArg = args.length > 1 ? args[1] : null;
+        @Nullable Duration duration = null;
+        if (durationArg != null) try {
+            duration = Duration.parse(durationArg);
+        }
+        catch (DateTimeParseException ignored) {}
+
+        if (duration != null && (duration.isNegative() || duration.isZero()))
+            return sendMessage(sender, SMPCore.messages().errorDurationZeroOrLess());
+
+        final @Nullable Date banExpiry = duration == null ? null : Date.from(Instant.now().plus(duration));
+
+        final @Nullable String reason = args.length > 1 ? String.join(" ", Arrays.copyOfRange(args, duration == null ? 1 : 2, args.length)) : null;
         final @NotNull NamespacedKey banSource;
         if (sender instanceof final @NotNull Player player)
             banSource = new NamespacedKey(SMPCore.getInstance(), "player/" + player.getUniqueId());
         else banSource = new NamespacedKey(SMPCore.getInstance(), "console");
 
+        final @NotNull OfflinePlayer target = SMPCore.getInstance().getServer().getOfflinePlayer(args[0]);
         final @NotNull Optional<@NotNull Member> targetMember = Member.get(target);
         if (targetMember.isEmpty()) {
             SMPCore.runMain(() -> target.ban(reason, banExpiry, banSource.asString()));
-            return sendMessage(sender, SMPCore.messages().bannedPlayer(target));
+            return sendMessage(sender, SMPCore.messages().bannedPlayer(target, duration));
         }
         final @NotNull Member main = targetMember.get().altOwner().orElse(targetMember.get());
         final @NotNull HashSet<@NotNull Member> alts = main.getAlts();
 
         SMPCore.runMain(() -> main.player().ban(reason, banExpiry, banSource.asString()));
-        if (alts.isEmpty()) return sendMessage(sender, SMPCore.messages().bannedMember(main));
+        if (alts.isEmpty()) return sendMessage(sender, SMPCore.messages().bannedMember(main, duration));
         else {
             SMPCore.runMain(() -> {
                 for (final @NotNull Member alt : alts)
                     alt.player().ban(reason, banExpiry, banSource.asString());
             });
-            return sendMessage(sender, SMPCore.messages().bannedMemberChain(main, alts.stream().toList()));
+            return sendMessage(sender, SMPCore.messages().bannedMemberChain(main, alts.stream().toList(), duration));
         }
     }
 
